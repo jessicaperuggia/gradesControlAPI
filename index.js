13964c2535a649f3dc01a1c652910b385c8ce9bf
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-08-06 19:49:01 -0300

    creating API

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import gradesRouter from './routes/grades.js';
+import { promises as fs } from 'fs';
+
+const { readFile, writeFile } = fs;
+
+const app = express();
+app.use(express.json());
+
+app.use('/grade', gradesRouter);
+
+app.listen(3000, async () => {
+    try {
+        await readFile('grades.json');
+        console.log('API Started');
+    } catch (err) {
+        const initialJson = {
+            nextId: 1,
+            grades: []
+        }
+        writeFile('grades.json', JSON.stringify(initialJson)).then(() => {
+            console.log('API Started and File Created');
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+});
