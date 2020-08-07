6d86f7acdd74b2b7bfbeb6857906db62041e14e1
Jessica Peruggia <jessica.peruggia@hotail.com>
2020-08-06 22:36:03 -0300

    get method

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { promises as fs } from "fs";
 
 const { readFile, writeFile } = fs;
 
+global.fileName = "grades.json";
+
 const app = express();
 app.use(express.json());
 
@@ -11,14 +13,14 @@ app.use("/grade", gradesRouter);
 
 app.listen(3000, async () => {
     try {
-        await readFile("grades.json");
+        await readFile("global.fileName");
         console.log("API Started");
     } catch (err) {
         const initialJson = {
             nextId: 1,
             grades: []
         }
-        writeFile("grades.json", JSON.stringify(initialJson)).then(() => {
+        writeFile("global.fileName", JSON.stringify(initialJson)).then(() => {
             console.log("API Started and File Created");
         }).catch(err => {
             console.log(err);
--- routes/grades.js
+++ routes/grades.js
@@ -4,20 +4,32 @@ import { promises as fs } from "fs";
 const { readFile, writeFile } = fs;
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));
+        delete data.nextId;
+        res.send(data);
+    } catch (err) {
+        res.status(400).send({ error: err.message });
+    }
+});
+
 router.post("/", async (req, res) => {
     try {
         let grade = req.body;
-        const data = JSON.parse(await readFile("grades.json"));
+        const data = JSON.parse(await readFile("global.fileName"));
 
         grade = { id: data.nextId++, ...grade };
         data.grades.push(grade);
 
-        await writeFile("grades.json", JSON.stringify(data, null, 2));
+        await writeFile("global.fileName", JSON.stringify(data, null, 2));
 
         res.send(grade);
     } catch (err) {
         res.status(400).send({ error: err.message });
     }
 });
+
+
 
 export default router;
